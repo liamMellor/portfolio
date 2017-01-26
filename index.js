9f719a68132119bff0975802780b700b97bbf64f
liamMellor <liam.r.mellor@gmail.com>
2017-01-26 01:29:09 -0500

    added GA tracking and removed unecessary js.

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
 window.scrollTo(0,0);
-var rowsContainer = document.getElementsByClassName('projects__rows');
-var rowsContainer = rowsContainer[0];
-rowsContainer.addEventListener("mouseover", function() {
-	rowsContainer.classList.add('projects__rows--active');
-});
-rowsContainer.addEventListener("mouseleave", function() {
-	rowsContainer.classList.remove('projects__rows--active');
-});
-var rows = document.getElementsByClassName('projects__row');
-Array.prototype.forEach.call(rows, function(element) {
-	element.addEventListener("mouseover", function() {
-		element.classList.add('projects__row--active');
-	});
-	element.addEventListener("mouseleave", function() {
-		element.classList.remove('projects__row--active');
-	});
-});
